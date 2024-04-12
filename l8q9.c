df7769f4480e07a8b6b7c0f71fa9b4dabe06eb70
pdrlcscrdr <166539206+pdrlcscrdr@users.noreply.github.com>
2024-04-12 10:14:53 -0300

    Exemplo de código que utiliza uma funções com retorno e sem retorno.
    Calcula a média de um aluno e informa seu conceito.

---
+++ l8q9.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<locale.h>
+
+float media(float n1, float n2, float n3);
+void conceito(float m);
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	float n1, n2, n3, m;
+	printf("Digite as três notas para o cálculo da média: \n");
+	scanf("%f %f %f", &n1, &n2, &n3);
+	m = media(n1, n2, n3);
+	printf("A média é: %.1f. \n", m);
+	conceito(m);
+	return(0);
+}
+
+float media(float n1, float n2, float n3){
+	return((n1 + n2 + n3) / 3);
+}
+
+void conceito(float m){
+	if(m >= 9){
+		printf("Conceito: A.");
+	}else if(m >= 7 && m < 9){
+		printf("Conceito: B.");
+	}
+	else if(m >= 6 && m < 7){
+		printf("Conceito: C.");
+	}
+	else if(m >= 4 && m < 6){
+		printf("Conceito: D.");
+	}
+	else{
+		printf("Conceito: E.");
+	}
+}
