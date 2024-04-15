68338ae7b1be6653206440e52ded3003f77b34d4
Pedro <166539206+pedrolucascrdr@users.noreply.github.com>
2024-04-15 15:29:43 -0300

    Função que verifica se um número é zero.c

--- Função que verifica se um número é zero.c
+++ Função que verifica se um número é zero.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<locale.h>
-
+//Protótipo da função
 void zon(int i);
 
 int main(){
 	setlocale(LC_ALL, "Portuguese");
 	int x;
+	//Solicitação de um valor ao usuário
 	printf("Digite um valor inteiro: \n");
 	scanf("%i", &x);
+	//Chamada da função
 	zon(x);
 	return(0);
 }
-
+//Definição da função
 void zon(int i){
 	if(i != 0){
 		printf("%i não é igual a zero.", i);
